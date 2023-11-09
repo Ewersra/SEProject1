221a8335d10546e3c83a37e32d2a990313190544
Ewersra <145415567+Ewersra@users.noreply.github.com>
2023-11-09 08:38:38 -0600

    Update run.py

--- CS325_p3/run.py
+++ CS325_p3/run.py
@@ -1,15 +1,20 @@
 import sys
-#import os.path
-import module_1
-from getsite import getSite
-sys.path.insert(0, '\CS325_p3\module_1')
-#from getdata import getHTML
-#from getcomments import getCleanData
+from module_1.getsite import getSite
+from module_2.getdata.py import getHTML
+from module_3.getcomments.py import getCleanData
+from module_4.project4 import
 
+#get the reddit URL from the command line
 url = sys.argv[1]
 
+#sends the url to getsite, which requests the website
 website = getsite.getSite(url)
 
+#sends the website data to getdata, which grabs the html from the page
 data = getdata.getHTML(website)
 
+#sends the page html to getcomments, which grabs the comments from the html, returns a file of comments
 comments = getcomments.getCleanData(data)
+
+#sends the comments to project4, which sends a prompt with the comments to ChatGPT to get the sentiments, returns a file of sentiments
+sentiments = 
