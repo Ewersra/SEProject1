7f4afe4c939123b6ed1fa9b576823e1378db56a5
Ewersra <145415567+Ewersra@users.noreply.github.com>
2023-12-04 11:09:35 -0600

    Update getsite.py

--- project 5/module_1/getsite.py
+++ project 5/module_1/getsite.py
@@ -3,6 +3,10 @@
 
 import requests
 
-def getSite(url):
-    website = requests.get(url)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'
+}
+
+def getSite(url, headers):
+    website = requests.get(url, headers=headers)
     return website
