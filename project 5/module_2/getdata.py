00b7b1ef6841f3f1893b5811098c3176b3e4b153
Ewersra <145415567+Ewersra@users.noreply.github.com>
2023-12-04 11:09:59 -0600

    Update getdata.py

--- project 5/module_2/getdata.py
+++ project 5/module_2/getdata.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 def getHTML(website, outfile):
     soup = BeautifulSoup(website.content, 'html.parser')
-    outputFile = open(outfile, 'w')
+    outputFile = open(outfile, 'w', encoding='utf-8')
     print(soup.prettify(), file = outputFile)
     outputFile.close()
     return soup.prettify()
