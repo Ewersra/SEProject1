a530b6020df1f5db59f40b381a0ba84a63a0e7b7
Ewersra <145415567+Ewersra@users.noreply.github.com>
2023-12-04 11:57:30 -0600

    Update getdata.py

--- project 5/module_2/getdata.py
+++ project 5/module_2/getdata.py
@@ -4,8 +4,11 @@
 from bs4 import BeautifulSoup
 
 def getHTML(website, outfile):
+    #parse the html file with beaitiful soup
     soup = BeautifulSoup(website.content, 'html.parser')
+    #open output file
     outputFile = open(outfile, 'w', encoding='utf-8')
+    #remove the html tages and print
     print(soup.prettify(), file = outputFile)
     outputFile.close()
     return soup.prettify()
