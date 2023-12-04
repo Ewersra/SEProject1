7290dcaf87616c076fe2a429fb6567662a642d6a
Ewersra <145415567+Ewersra@users.noreply.github.com>
2023-12-04 11:56:20 -0600

    Update getsite.py

--- project 5/module_1/getsite.py
+++ project 5/module_1/getsite.py
@@ -3,6 +3,7 @@
 
 import requests
 
+#Set user agent so reddit doesn't block the program for being a bot
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36'
 }
