878d9f579e611af74cc1ac245627206b8e2d873a
Ewersra <145415567+Ewersra@users.noreply.github.com>
2023-12-04 11:59:45 -0600

    Update getcomments.py

--- project 5/module_3/getcomments.py
+++ project 5/module_3/getcomments.py
@@ -8,12 +8,16 @@ def getCleanData(data, outfile):
 
     outputFile = open(outfile, 'w')
 
+    #parse html with soup
     soup = BeautifulSoup(data, 'html.parser')
 
+    #find comment space using the html tages
     comments = soup.find('div', {'class': 'commentarea'})
-
+    
+    #find comments using tags
     comment_content = comments.find_all('div', {'class': 'comment'})
 
+    #print comments and strip white space, add a newline after comments
     for comment in comment_content:
         comment_text = comment.find('div', {'class': 'md'}).text.strip()
         print(comment_text + '\n', file = outputFile)
